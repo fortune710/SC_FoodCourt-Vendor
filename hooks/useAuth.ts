5247934c20963f18e7c668daf5f3eb5eaf99b059
fortune710 <fortune0208@yahoo.com>
2024-04-29 19:40:08 -0400

    intergrated supabase

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -1,19 +1,23 @@
 import { createUserWithEmailAndPassword, getIdToken, signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../utils/firebase';
+import { supabase } from '../utils/supabase';
 
 export default function useAuth() {
     
     const signIn = async (email: string, password: string) => {
-        const { user } = await signInWithEmailAndPassword(auth, email, password);
-        const idToken = await getIdToken(user);
-        return idToken
+        const res = await supabase.auth.signInWithPassword({
+            email,
+            password,
+        })
+        
+        return ""
     }
 
     const signUpWithEmail = async (email: string, password: string) => {
-        const { user } = await createUserWithEmailAndPassword(auth, email, password);
-        const idToken = await getIdToken(user);
-        return idToken
-
+        const res = await supabase.auth.signUp({
+            email, password
+        })
+        return res.data.user
     }
 
     return {
