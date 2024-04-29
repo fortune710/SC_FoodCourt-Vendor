bdbc36f19f8bb363bfd9f9e334b72d58a510c136
fortune710 <fortune0208@yahoo.com>
2024-04-29 19:40:58 -0400

    added option for safearea and page background

--- components/page.tsx
+++ components/page.tsx
@@ -1,15 +1,27 @@
 import React from "react";
-import { Dimensions, ScrollView, StyleSheet } from "react-native";
+import { Dimensions, ScrollView, StyleProp, StyleSheet, ViewStyle } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
+/**
+ * Interface for PageProps
+ * @interface
+ * @property {React.ReactNode} children - The children nodes of the Page component.
+ * @property {string} safeAreaBgColor - The background color for the safe area. This is only neccesary on iOS.
+ */
 interface PageProps {
     children: React.ReactNode;
+    safeAreaBgColor?: string;
+    style?: StyleProp<ViewStyle>;
 }
 
-export default function Page({ children }: PageProps) {
+export default function Page({ children, style, safeAreaBgColor }: PageProps) {
+    const safeArea = {
+        backgroundColor: safeAreaBgColor || "#fff"
+    }
+    
     return (
-        <SafeAreaView>
-            <ScrollView style={styles.pageStyle}>
+        <SafeAreaView style={safeArea}>
+            <ScrollView style={[styles.pageStyle, style ?? {}]}>
                 { children }
             </ScrollView>
         </SafeAreaView>
@@ -20,6 +32,7 @@ const styles = StyleSheet.create({
     pageStyle: {
         width: Dimensions.get('screen').width,
         height: Dimensions.get('screen').height,
-        position: 'relative'
-    }
+        position: 'relative',
+        backgroundColor: "white"
+    },
 })
