d367daa16cd07937911b8b4e16e162403ffe99ae
fortune710 <fortune0208@yahoo.com>
2024-08-29 18:44:10 -0400

    feat: enabled search

--- app/menu/index.tsx
+++ app/menu/index.tsx
@@ -1,27 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import Page from '../../components/page'
 import { ActivityIndicator, Pressable, Text, TouchableOpacity, View } from "react-native";
 import CategoryListItem from '../../components/category-list-item';
 import { CategoryListItemProps, MenuItem } from "../../utils/types";
 import Header from "~/components/header";
-import { Plus } from "lucide-react-native";
+import { Plus, Search } from "lucide-react-native";
 import useThemeColor from "~/hooks/useThemeColor";
 import { Link } from "expo-router";
 import useMenuItems from "~/hooks/useMenuItems";
+import { Input } from "@rneui/themed";
 
 export default function MenuPage(){
     const primary = useThemeColor({}, "primary");
-    const { menuItems, isLoading } = useMenuItems();
+    const { menuItems, isLoading, menuItemsRaw } = useMenuItems();
+    const [mode, setMode] = useState<"search"|"list">("list");
     const availableCategories = Object.keys(menuItems ?? {});
+
+    if (mode === "search") {
+        return (
+            <Page>
+                <View className="flex flex-row items-center justify-between px-3">
+                    <Input/>
+
+                    <TouchableOpacity onPress={() => setMode("list")}>
+                        <Text>Cancel</Text>
+                    </TouchableOpacity>
+                </View>
+                <View>
+                    {
+                        menuItemsRaw?.map((menuItem: MenuItem) => (
+                            <CategoryListItem
+                                foodName={menuItem.name}
+                                price={menuItem.price}
+                                addons={menuItem?.add_ons!}
+                                id={menuItem.id}
+                                category={menuItem.category}
+                                key={menuItem.id}
+                            />
+                        ))
+                    }
+                </View>
+
+            </Page>
+        )
+    }
 
 
     return (
         <Page>
             <Header 
                 rightIcon={
-                    <Link href="/menu/create">
-                        <Plus size={30} stroke={primary}/>
-                    </Link>
+                    <View className="flex flex-row gap-4 items-center">
+                        <TouchableOpacity onPress={() => setMode("search")}>
+                            <Search size={30} stroke={primary}/>
+                        </TouchableOpacity>
+
+                        <Link href="/menu/create">
+                            <Plus size={30} stroke={primary}/>
+                        </Link>
+                    </View>
                 }
                 headerTitle="Menu" style="dark"/>
             <View className="px-4">
