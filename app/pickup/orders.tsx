f3f91275bc8af3b977c077cbb1b2342b38ebd461
fortune710 <fortune0208@yahoo.com>
2024-09-02 16:58:46 -0400

    fetched pick up orders

--- app/pickup/orders.tsx
+++ app/pickup/orders.tsx
@@ -1,22 +1,36 @@
 import OrderCard from "../../components/orders/order-card";
 import Page from "../../components/page";
 import OrderCardDetails from "../../components/orders/order-card-details";
-import { View } from "react-native";
+import { ActivityIndicator, View } from "react-native";
 import { Button } from "@rneui/themed";
 import Header from "../../components/page-header";
+import { useLocalSearchParams } from "expo-router";
+import useOrderPickup from "~/hooks/useOrderPickup";
 
 export default function PickupOrders() {
+    const { order_nums } = useLocalSearchParams();
+    const orderIds = (order_nums as string[]).map((order_num) => parseInt(order_num));
+
+    const { orders, isLoading, markOrderAsCollected } = useOrderPickup(orderIds)
+
     return (
         <Page>
             <Header headerTitle="Pickup" /> 
-            <OrderCard>
-                <OrderCardDetails/>
-                <View>
-                    <Button>
-                        Mark as Collected
-                    </Button>
-                </View>
-            </OrderCard>
+            {
+                isLoading ? <ActivityIndicator/> :
+                orders?.map((order) => {
+                    return (
+                        <OrderCard>
+                            <OrderCardDetails/>
+                            <View>
+                                <Button onPress={() => markOrderAsCollected(order?.id)}>
+                                    Mark as Collected
+                                </Button>
+                            </View>
+                        </OrderCard>
+                    )
+                })
+            }
         </Page>
     )
 }
