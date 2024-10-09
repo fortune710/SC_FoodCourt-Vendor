71207097cae92dc6e9f43e7f2c1834ebc67f9ea7
fortune710 <fortune0208@yahoo.com>
2024-10-09 16:29:47 -0400

    added new properties and new types for resturants

--- utils/types.ts
+++ utils/types.ts
@@ -21,11 +21,21 @@ export interface CreateMenuItemData {
 }
 
 export interface ResturantData {
+    id?: string,
     admin_id: string,
     name: string,
     phone_number?: string,
     website_link?: string,
-    is_closed?: boolean
+    is_closed?: boolean,
+    subaccount_code?: string,
+    account_number?: string
+}
+
+export interface ICreateSubaccount {
+    bank_code: string,
+    account_number: string,
+    percentage_charge: number,
+    business_name: string
 }
 
 export interface MenuItem extends CreateMenuItemData {
