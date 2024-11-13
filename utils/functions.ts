04ae30442076a9659e4716af98edb0aa23c45db6
Fortune Oluwasemilore Alebiosu <fortune0208@yahoo.com>
2024-11-12 22:43:36 -0500

    added image picker

--- utils/functions.ts
+++ utils/functions.ts
@@ -1,3 +1,5 @@
+import { decode } from "base64-arraybuffer";
+
 export function groupArrayBy(array: any[], key: string) {
   return array?.reduce((result, currentItem) => {
     // Get the value of the key we're grouping by
@@ -19,4 +21,20 @@ export function canShowAdminMenu(userType: string, vendorView: boolean) {
   if (userType === "admin" && !vendorView) return true;
 
   return false;
+}
+
+
+export function decodeBase64(base64: string) {
+  const base64Str = base64.includes("base64,")
+  ? base64.substring(
+      base64.indexOf("base64,") + "base64,".length
+    )
+  : base64;
+  const res = decode(base64Str);
+
+  if (!(res.byteLength > 0)) {
+    console.error("[uploadToSupabase] ArrayBuffer is null");
+    return null;
+  }
+  return res
 }
