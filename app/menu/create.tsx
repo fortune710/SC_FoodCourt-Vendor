4e86ca845171130bd6fb0b679a25ac8a045dc120
DevUtee <usieredev@gmail.com>
2025-01-22 00:50:51 +0100

    Button disabled color

--- app/menu/create.tsx
+++ app/menu/create.tsx
@@ -165,8 +165,8 @@ export default function CreateMenuItemPage() {
 
                             <AlertDialog>
                                 <AlertDialogTrigger asChild>
-                                    <ShadcnButton className="rounded-full h-7 w-7 p-2" size="icon">
-                                        <Plus className="w-2 h-2" stroke="white"/>
+                                    <ShadcnButton  size="icon" style={{width: 30, height: 30, borderRadius: 30/2, padding: 4}}>
+                                        <Plus stroke="white" style={{height: 24}}/>
                                     </ShadcnButton>
                                 </AlertDialogTrigger>
 
